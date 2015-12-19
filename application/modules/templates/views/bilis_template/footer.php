31863420697798622b36dadba550140d7f68b2df
gerardkristoffer <gerardkristoffer.sabado@gmail.com>
2015-12-19 20:02:12 +0800

    fix bootstrap modal issue in phone

--- application/modules/templates/views/bilis_template/footer.php
+++ application/modules/templates/views/bilis_template/footer.php
@@ -16,20 +16,16 @@
     <script src="<?php echo base_url() ?>assets/libs/slimScroll/jquery.slimscroll.min.js"></script>
     <!-- FastClick -->
     <script src="<?php echo base_url() ?>assets/libs/fastclick/fastclick.min.js"></script>
-
     <!-- dataTables -->
     <script src="<?php echo base_url() ?>assets/libs/dataTables/js/jquery.dataTables.min.js"></script>
     <script src="<?php echo base_url() ?>assets/libs/dataTables/js/dataTables.bootstrap.js"></script>
     <script src="<?php echo base_url() ?>assets/libs/dataTables/js/dataTables.responsive.js"></script>  
-
+    <!-- datepicker -->
     <script src="<?php echo base_url() ?>assets/libs/sweetalert2/sweetalert2.min.js"></script>  
     <script src="<?php echo base_url() ?>assets/libs/datepicker/bootstrap-datepicker.js"></script> 
-
-
-    
     <!-- Select2 -->
     <script src="<?php echo base_url(); ?>assets/libs/select2/js/select2.full.min.js"></script>
-
+    
     <!-- AdminLTE App -->
     <script src="<?php echo base_url() ?>assets/libs/theme/js/app.min.js"></script>
     <!-- AdminLTE for demo purposes -->
@@ -38,15 +34,19 @@
     <script src="<?php echo base_url() ?>assets/js/admin/register.js"></script>
 
     <script>
-        jQuery('.table-res').DataTable({
+        var table = jQuery('.table-res').DataTable({
             scrollY:        "410px",
             scrollCollapse: false,
             paging:         false,
             responsive:     true
         });
-        jQuery(".editModal").click(function(){
+        $('.table-res tbody').on('click', 'button', function () {
             jQuery("#editModalWindow").modal({backdrop: 'static'});
         });
+        // jQuery("#editModal").click(function(){
+        //     alert("ppsh");
+        //     jQuery("#editModalWindow").modal({backdrop: 'static'});
+        // });
         jQuery(".select2").select2({
             placeholder: "Select a vehicle",
             allowClear: true
