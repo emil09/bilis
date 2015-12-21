ec0069447093459dfd4f55071f502d54a2f08d2d
Gerard <gerardkristoffer.sabado@gmail.com>
2015-12-21 15:47:01 +0800

    added activetrips and scheduling pages

---
+++ application/modules/dispatcher/controllers/activetrips.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+Class ActiveTrips extends MY_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->check_session_dispatcher();
+	}
+
+	public function index()
+	{
+		$data['module'] = 'dispatcher';
+		$data['view_file'] = 'activetrips_view';
+		$data['sidebar'] = 'dispatcher/dispatcher_sidebar';
+
+		$data['css'] = $this->add_css(array(FooTableCSS, Select2CSS));
+		$data['js'] = $this->add_js(array(FooTableJS, FooTableSortJS, FooTableFilterJS, Select2JS));		
+		
+		echo Modules::run('templates/bilis_template', $data);
+	}
+
+}
--- application/modules/dispatcher/views/dispatcher_sidebar.php
+++ application/modules/dispatcher/views/dispatcher_sidebar.php
@@ -2,8 +2,13 @@
   <li class="header">MAIN NAVIGATION</li>
   <li class="<?php if($this->uri->segment(2)=='dashboard'){echo 'active';} ?>"><a href="dashboard"><i class="fa fa-home"></i><span>Home</span></a></li>
   <li class="<?php if($this->uri->segment(2)=='available'){echo 'active';} ?>"><a href="available"><i class="fa fa-pause"></i><span>Available for Dispatch</span></a></li>
-  <li><a href="dispatch.php"><i class="fa fa-play"></i> <span>Active Trips</span></a></li>
-  <li><a href="dispatch.php"><i class="fa fa-calendar"></i> <span>Scheduling</span></a></li>
+  <li class="<?php if($this->uri->segment(2)=='activetrips'){echo 'active';} ?>"><a href="activetrips"><i class="fa fa-play"></i> <span>Active Trips</span></a></li>
+  <li class="treeview <?php if($this->uri->segment(2)=='schedulinglast'){echo 'active';} ?>"><a href="#"><i class="fa fa-calendar"></i> <span>Scheduling</span><i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+      <li><a href="schedulinglast"><i class="fa fa-backward"></i> Previous Days</a></li>
+      <li><a href="schedulingnext"><i class="fa fa-forward"></i> Next 7 Days</a></li>
+    </ul>
+  </li>
   <li><a href="dispatch.php"><i class="fa fa-clone"></i> <span>Dispatch Report</span></a></li>
   <!-- <li class="header">LABELS</li>
   <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
