81de136dbf2bbca9061ecac451f49e498071f006
Gerard <gerardkristoffer.sabado@gmail.com>
2016-01-08 10:18:07 +0800

    cashier module

--- application/modules/cashier/views/cashier_sidebar.php
+++ application/modules/cashier/views/cashier_sidebar.php
@@ -4,13 +4,5 @@
     <li class="<?php if($this->uri->segment(2)=='cashturnover'){echo 'active';} ?>"><a href="<?php echo base_url() ?>dispatcher/cashturnover">Cash Turnover</a></li>
 
     <li class="<?php if($this->uri->segment(2)=='turnoverreport'){echo 'active';} ?>"><a href="<?php echo base_url() ?>dispatcher/turnoverreport">Turnover Report</a></li>
-
-    <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Scheduling <span class="caret"></span></a>
-      <ul class="dropdown-menu" role="menu">
-        <li><a href="<?php echo base_url() ?>dispatcher/schedulinglast">Previous 7 Days</a></li>
-        <li><a href="<?php echo base_url() ?>">Next 7 Days</a></li>
-      </ul>
-    </li>
   </ul>
 </div>
